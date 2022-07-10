b1de023d93b3684ae04e53c6ed7970a812c2f3f3
Everton Solon Tiba <evertonsolon@outlook.com>
2022-07-10 12:05:19 -0300

    Testando a ordenação das rotas

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -28,20 +28,32 @@ app.UseSession();
 
 app.UseAuthorization();
 
+//app.MapControllerRoute(
+//    name: "teste",
+//    pattern: "testeme",
+//    defaults: new { controller = "teste", Action = "Index"});
+
+//app.MapControllerRoute(
+//    name: "admin",
+//    pattern: "admin/{action=Index}/{id?}",
+//    defaults: new { controller = "Admin" });
+
+//A ordenação das rotas é muito importante!
+//Nos exemplos abaixo, se digitar admin na url, a primeira condição irá atender e não irá para admin.
+//Já se inverter a ordem das rotas nesta configuração e colocar a admin antes da home, ao digitar admin
+//irá para o controller Admin.
 app.MapControllerRoute(
-    name: "teste",
-    pattern: "testeme",
-    defaults: new { controller = "teste", Action = "Index"});
+    name: "home",
+    pattern: "{home}",
+    defaults: new { controller = "Home", Action = "Index" });
 
 app.MapControllerRoute(
     name: "admin",
-    pattern: "admin/{action=Index}/{id?}",
-    defaults: new { controller = "Admin" });
+    pattern: "admin",
+    defaults: new { controller = "Admin", Action = "Index" });
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
 
 app.Run();
