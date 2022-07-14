2376152ec5e7ad0470cbc1c41ff8a5d201b7e549
Everton Solon Tiba <evertonsolon@outlook.com>
2022-07-14 15:14:37 -0300

    Outra forma de implementar a autorização de acesso ao sistema.

--- LanchesMac/Controllers/ContatoController.cs
+++ LanchesMac/Controllers/ContatoController.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
+    [Authorize]
     public class ContatoController : Controller
     {
         public IActionResult Index()
         {
-            if (User.Identity.IsAuthenticated) { return View(); }
-            
-            return RedirectToAction("Login", "Account");
+            //if (User.Identity.IsAuthenticated) { return View(); }
+
+            //return RedirectToAction("Login", "Account");
+            return View();
         }
     }
 }
